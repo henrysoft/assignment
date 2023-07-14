e813512429b44310f21acab4945573ab4c2ba1a0
henrysoft <princehenry2012@yahoo.com>
2023-07-14 17:19:17 +0100

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        double num3 = 7.5;
+        double num4 = 2.5;
+
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int difference = num1 - num2;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+
+        // Division
+        double quotient = num3 / num4;
+        System.out.println("Quotient: " + quotient);
+
+        // Modulo (Remainder)
+        int remainder = num1 % num2;
+        System.out.println("Remainder: " + remainder);
+    }
+}
